9cacda29f8ef6671edc75a42ade76838620ffbbc
David Vaughan <dvaug86@gmail.com>
2023-12-17 12:52:19 -0500

    forgot to save before last commit

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const {Configuration, OpenAIapi} = require('openai')
+
+
+const config = new Configuration({
+    apiKey:""
+    
+})
